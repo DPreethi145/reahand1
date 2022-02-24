89ec54c03cbebe69895d5e60b737b4abc8f81ac1
DPreethi145 <94468332+DPreethi145@users.noreply.github.com>
2022-02-24 12:48:15 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1 className='head'>STYLING USING CLASS AND FUNCTIONAL COMPONENT</h1>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
